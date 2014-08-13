c593063266b65c16fad2c4378da716e9c529f129
dcduncan <dcduncan@ucsd.edu>
2014-08-13 09:03:03 -0700

    DD - Adds the ActionListener for the buttons.

---
+++ WheelOfPunishment/src/com/ui/ButtonListener.java
@@ -0,0 +1,19 @@
+package com.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Listens to the buttons from the GUI.
+ *
+ * @author niklj_000
+ */
+public class ButtonListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(final ActionEvent arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
