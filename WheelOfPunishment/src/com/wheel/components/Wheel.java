22ffead085d8d23ee8b19c7fa06dafe08a4c75a9
dcduncan <dcduncan@ucsd.edu>
2014-08-15 08:18:22 -0700

    DD - Adds the Wheel class.

---
+++ WheelOfPunishment/src/com/wheel/components/Wheel.java
@@ -0,0 +1,11 @@
+package com.wheel.components;
+
+/**
+ * The wheel is the main component of the Wheel of Punishment. It is varying colors, and has a
+ * variety of texts, it can spin and notify what it has landed on.
+ *
+ * @author niklj_000
+ */
+public class Wheel {
+
+}
