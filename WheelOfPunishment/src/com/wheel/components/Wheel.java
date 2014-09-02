dcac15bd5907d5c69eab163434856026618455cb
dcduncan <dcduncan@ucsd.edu>
2014-09-02 08:57:48 -0700

    DD - Splits up drawing responsibility between 3 methods for drawing, getting color, and getting random colors.

--- WheelOfPunishment/src/com/wheel/components/Wheel.java
+++ WheelOfPunishment/src/com/wheel/components/Wheel.java
@@ -13,11 +13,45 @@ import javax.swing.JComponent;
  */
 public final class Wheel extends JComponent {
 
+
     /**
      * Allows user to edit the options of the wheel.
      */
     public void edit() {
         System.err.println("Edit");
+    }
+
+    /**
+     * Returns a color for the wheel.
+     *
+     * @param currentSection Current section of the wheel it is coloring.
+     * @return Color, the color for the section.
+     */
+    private Color getColor(final int currentSection) {
+        switch (currentSection) {
+            case 0:
+                return Color.RED;
+            case 1:
+                return Color.ORANGE;
+            case 2:
+                return Color.YELLOW;
+            case 3:
+                return Color.GREEN;
+            case 4:
+                return Color.BLUE;
+            case 5:
+                return Color.PINK;
+            case 6:
+                return Color.MAGENTA;
+            case 7:
+                return Color.CYAN;
+            case 8:
+                return Color.GRAY;
+            case 9:
+                return Color.BLACK;
+            default:
+                return randomColor(currentSection);
+        }
     }
 
     /**
@@ -32,43 +66,29 @@ public final class Wheel extends JComponent {
         final int height = 100;
 
         final int degreesInCircle = 360;
-        final int numberOfSections = 7;
+        final int numberOfSections = 11;
         final int delta = (int) Math.ceil((double) degreesInCircle / numberOfSections);
 
         final int xPos = (int) (g.getClipBounds().getWidth() - width) / 2;
         final int yPos = (int) (g.getClipBounds().getHeight() - height) / 2;
 
         int currentSection = 0;
+        do {
+            g.setColor(getColor(currentSection));
+            g.fillArc(
+                    xPos, yPos, width, height, currentSection * delta, delta);
+            ++currentSection;
+        } while (currentSection < numberOfSections);
+    }
 
-        g.setColor(Color.RED);
-        g.fillArc(
-                xPos, yPos, width, height, currentSection * delta, delta);
-        ++currentSection;
-        g.setColor(Color.ORANGE);
-        g.fillArc(
-                xPos, yPos, width, height, currentSection * delta, delta);
-        ++currentSection;
-        g.setColor(Color.YELLOW);
-        g.fillArc(
-                xPos, yPos, width, height, currentSection * delta, delta);
-        ++currentSection;
-        g.setColor(Color.GREEN);
-        g.fillArc(
-                xPos, yPos, width, height, currentSection * delta, delta);
-        ++currentSection;
-        g.setColor(Color.BLUE);
-        g.fillArc(
-                xPos, yPos, width, height, currentSection * delta, delta);
-        ++currentSection;
-        g.setColor(Color.PINK);
-        g.fillArc(
-                xPos, yPos, width, height, currentSection * delta, delta);
-        ++currentSection;
-        g.setColor(Color.GRAY);
-        g.fillArc(
-                xPos, yPos, width, height, currentSection * delta, delta);
-
-
+    /**
+     * Returns a random color with the seed.
+     *
+     * @param seed int that will seed for the random color.
+     * @return Color, for coloring the wheel.
+     */
+    private Color randomColor(final int seed) {
+        return Color.WHITE;
     }
 
     /**
