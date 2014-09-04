f6ae4f97c839b5f3417487af6644e1e20f6a48e0
dcduncan <dcduncan@ucsd.edu>
2014-09-03 19:03:30 -0700

    DD - Separates the listeners into their own package.

---
+++ WheelOfPunishment/src/com/listeners/ButtonListener.java
@@ -0,0 +1,42 @@
+package com.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import com.ui.MainFrame;
+
+/**
+ * Listens to the buttons from the GUI.
+ *
+ * @author niklj_000
+ */
+public class ButtonListener implements ActionListener {
+
+    /**
+     * The Frame with the wheel and buttons and methods.
+     */
+    private MainFrame mainFrame = null;
+
+    /**
+     * The constructor take a MainFrame for where the wheel is called.
+     *
+     * @param frame MainFrame
+     */
+    public ButtonListener(final MainFrame frame) {
+        mainFrame = frame;
+    }
+
+    @Override
+    public final void actionPerformed(final ActionEvent arg0) {
+        // TODO Auto-generated method stub
+
+        if (arg0.getActionCommand().matches(
+                ".*Spin.*")) {
+            mainFrame.spinWheel();
+        } else if (arg0.getActionCommand().matches(
+                ".*Edit.*")) {
+            mainFrame.editWheelOptions();
+        }
+
+    }
+}
